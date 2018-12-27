d990f940af227c3ccad65b4b3d6f70d946615c48
holycb <chernodub98@gmail.com>
2018-12-27 18:59:59 +0700

    получил дистанцию между 1 и 2 точкой без учёта дорог

---
+++ js/layout.js
@@ -0,0 +1,63 @@
+var sidebar;
+var currentLayout;
+var mapContainer; 
+
+function initMap() {
+  mapContainer = (document.createElement('div', {id: 'map'}));
+  mapContainer.id = 'map';
+  document.querySelector('.main-layout').appendChild(mapContainer);
+  // Y API
+  ymaps.ready(init); // calls init function from map.js
+  mapContainer = document.querySelector('#map');
+}
+
+function initButtons() {
+
+  document.querySelector('#side-bar-button').addEventListener('click', () => {
+    if (sidebar.isOpen) sidebar.close();
+    else sidebar.open();
+  });
+  
+  document.querySelectorAll('.navigation-button').forEach(element => {
+    element.addEventListener('click', el => {
+      let layout = el.target.id.replace('-button', '');
+      if (layout !== currentLayout) {
+        let layoutContainer = document.querySelector('.layout-container');
+  
+        while (layoutContainer.firstChild) {
+          layoutContainer.removeChild(layoutContainer.firstChild);
+        }
+  
+        var newLayout;
+        if (layout === 'map') {
+          newLayout = mapContainer;
+        } else {
+          newLayout = document.createElement('div');
+          newLayout.className = layout;
+        }
+  
+        layoutContainer.appendChild(newLayout);
+      }
+    });
+  });
+}
+
+function initSideNav() {
+  document.addEventListener('DOMContentLoaded', () => {
+    var elems = document.querySelector('.sidenav');
+    sidebar = M.Sidenav.init(elems);
+  });
+}
+
+function setMapMenu() {
+  
+}
+
+function initialLayout() {
+  initMap();
+  setMapMenu();
+}
+
+
+
+
---
+++ js/map.js
@@ -0,0 +1,86 @@
+var myMap;
+var multiRoute;
+
+function init() {
+  /**
+   * Creating a multiroute.
+   * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml
+   */
+  multiRoute = new ymaps.multiRouter.MultiRoute(
+    {
+      referencePoints: []
+    },
+    {
+      /**
+       * A type of intermediate points that can be added
+       * when editing.
+       */
+      editorMidPointsType: 'via',
+      // In the mode for adding new waypoints, we do not allow putting points on top of the map objects.
+      editorDrawOver: false
+    }
+  );
+
+  var buttonEditor = new ymaps.control.Button({
+    data: { content: 'Editing mode' }
+  });
+
+  buttonEditor.events.add('select', () => {
+    /**
+     * Enabling edit mode.
+     * As options, you can pass an object with fields:
+     * addWayPoints - Allows adding new waypoints by clicking on the map. Default value: false.
+     * dragWayPoints - Allows dragging existing waypoints. Default value: true.
+     * removeWayPoints - Allows deleting waypoints by double-clicking them. Default value: false.
+     * dragViaPoints - Allows dragging existing throughpoints. Default value: true.
+     * removeViaPoints - Allows deleting throughpoints by double-clicking them. Default value: true.
+     * addMidPoints - Allows adding intermediate points or waypoints by dragging the marker that appears when pointing the mouse at the active route. The type of points to add is set by the midPointsType option. Default value: true.
+     * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/multiRouter.MultiRoute.xml#editor
+     */
+    multiRoute.editor.start({
+      addWayPoints: true,
+      removeWayPoints: true
+    });
+  });
+
+  buttonEditor.events.add('deselect', () => {
+    // Turning off edit mode.
+    multiRoute.editor.stop();
+  });
+
+  ymaps.geolocation.get().then(res => {
+    myMap = new ymaps.Map(
+      'map',
+      {
+        center: res.geoObjects.position,
+        zoom: 10,
+        controls: [
+          'searchControl',
+          'typeSelector',
+          'fullscreenControl',
+          'zoomControl',
+          'geolocationControl',
+          buttonEditor
+        ]
+      },
+      {
+        buttonMaxWidth: 300
+      }
+    );
+
+    // Adding multiroute to the map.
+    myMap.geoObjects.add(multiRoute);
+  });
+}
+
+function getPoints() {
+  return multiRoute.model.getAllPoints().map(x => x.getReferencePoint());
+}
+
+function setBestWay() {
+  let points = getPoints();
+  console.log(points);
+  console.log(
+    ymaps.coordSystem.geo.getDistance(points[0], points[1])
+  );
+}
